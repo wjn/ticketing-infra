fb8431729ac492996a6e4fe4f6e9ab8e4a31e08c
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-09-19 08:18:05 -0500

    added Route Not Found error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,6 +7,7 @@ import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -15,6 +16,10 @@ app.use(currentUserRouter);
 app.use(signupRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
