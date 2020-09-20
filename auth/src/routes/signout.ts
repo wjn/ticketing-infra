765e047f5b8f6789f870be8ade9e632d8681745f
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-09-20 09:54:01 -0500

    signout complete

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-  res.send('Hi there, from the signout router!');
+  req.session = null;
+  res.send({});
 });
 
 export { router as signoutRouter };
